777e03a6aeaf946204637b4b703c4c3843a6ce13
Josh Batchelor <josh_batch@live.com.au>
2015-02-24 16:06:33 +0800

    -openworksofar

---
+++ hollywoo-db-server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express();
+var server = require('http').createServer(app);
+
+var port = 3004;
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use(express.static(__dirname + '/node_modules/d3'));
+
+app.get('/', function(req, res){
+	res.sendFile('public/index.html', {root: __dirname});
+});
+
+server.listen(port, function(){
+	console.log('listening on port ' + port)
+});
